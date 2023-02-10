705fdb2db1d5b24a771053e4d8782a189139f5ea
SlimeNull <slimenull@outlook.com>
2023-02-10 20:01:30 +0800

    忘记改名的改好了

---
+++ EleCho.GoCqHttpSdk/Action/CqSendPrivateForwardMessageAction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using EleCho.GoCqHttpSdk.Message;
+using EleCho.GoCqHttpSdk.Action.Model.Params;
+
+namespace EleCho.GoCqHttpSdk.Action
+{
+    /// <summary>
+    /// 发送私聊转发消息
+    /// </summary>
+    public class CqSendPrivateForwardMessageAction : CqAction
+    {
+        /// <summary>
+        /// 实例化对象
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="message"></param>
+        public CqSendPrivateForwardMessageAction(long userId, CqForwardMessage message)
+        {
+            UserId = userId;
+            ForwardMessage = message;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override CqActionType ActionType => CqActionType.SendPrivateForwardMessage;
+
+        /// <summary>
+        /// 用户 QQ
+        /// </summary>
+        public long UserId { get; set; }
+
+        /// <summary>
+        /// 转发消息
+        /// </summary>
+        public CqForwardMessage ForwardMessage { get; set; }
+
+        internal override CqActionParamsModel GetParamsModel()
+        {
+            return new CqSendPrivateForwardMsgActionParamsModel(UserId, ForwardMessage.Select(CqMsg.ToModel).ToArray());
+        }
+    }
+}
