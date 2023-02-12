4a29aa572ee5a0c4e3aa42807f574d0efbcb3b31
SlimeNull <slimenull@outlook.com>
2023-02-12 21:50:31 +0800

    docs: 补充 SDK 使用文档

--- src/TestConsole/Program.cs
+++ src/TestConsole/Program.cs
@@ -5,6 +5,7 @@ using System.Reflection.Metadata;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using EleCho.GoCqHttpSdk;
+using EleCho.GoCqHttpSdk.Action;
 using EleCho.GoCqHttpSdk.Message;
 using EleCho.GoCqHttpSdk.Post;
 //using EleCho.GoCqHttpSdk.MessageMatching;
@@ -26,6 +27,23 @@ namespace AssemblyCheck
 
         private static async Task Main(string[] args)
         {
+            CqSendPrivateMessageAction action = new CqSendPrivateMessageAction(114514, new CqMessage("qwq"));
+            CqActionResult? rst = await session.ActionSender.InvokeActionAsync(action);
+            CqSendPrivateMessageActionResult? msgRst = (CqSendPrivateMessageActionResult?)rst;
+
+            session.PostPipeline.Use(async (context, next) =>
+            {
+                if (context is CqGroupMessagePostContext groupMessagePostContext)
+                {
+                    await session.SendGroupMessageAsync(groupMessagePostContext.GroupId, new CqMessage("检测到这个群发送了一条消息"));
+                }
+            });
+
+            session.UseGroupMessage(async (context, next) =>
+            {
+                await session.SendGroupMessageAsync(context.GroupId, new CqMessage("检测到这个群发送了一条消息"));
+            });
+
             Console.WriteLine(CqPostSessionExtensionsCodeGen.Generate());
             return;
         }
