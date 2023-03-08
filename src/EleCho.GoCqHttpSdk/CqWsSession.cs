a2f8e7ba58afb7528b1db9edebe37f1f0339c75f
SlimeNull <slimenull@outlook.com>
2023-03-08 14:55:37 +0800

    docs: 添加一行注释

--- src/EleCho.GoCqHttpSdk/CqWsSession.cs
+++ src/EleCho.GoCqHttpSdk/CqWsSession.cs
@@ -324,6 +324,7 @@ namespace EleCho.GoCqHttpSdk
         /// <exception cref="InvalidOperationException">会话还没启动</exception>
         public async Task WaitForShutdownAsync()
         {
+            // 当 mainLoopTask 被赋值的时候, mainPostLoopTask 也会被赋值, 所以第二个条件基本不会执行
             if (mainLoopTask == null || mainPostLoopTask == null)
                 throw new InvalidOperationException("Session is not started yet");
 
