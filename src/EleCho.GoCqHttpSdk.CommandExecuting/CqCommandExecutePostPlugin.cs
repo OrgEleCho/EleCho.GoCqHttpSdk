87f46519a87fdc3e28d1c20e5727142d08d930c6
SlimeNull <slimenull@outlook.com>
2023-07-13 19:33:24 +0800

    feat: 指令拓展添加对 IEnumerable<CqMsg> 返回值的支持

--- src/EleCho.GoCqHttpSdk.CommandExecuting/CqCommandExecutePostPlugin.cs
+++ src/EleCho.GoCqHttpSdk.CommandExecuting/CqCommandExecutePostPlugin.cs
@@ -2,6 +2,8 @@
 using EleCho.GoCqHttpSdk.Message;
 using EleCho.GoCqHttpSdk.Post;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
@@ -26,6 +28,62 @@ namespace EleCho.GoCqHttpSdk.CommandExecuting
             return IgnoreCases ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal;
         }
 
+        private CqMessage BuildGroupResponse(object? result, long userId, long messageId)
+        {
+            CqMessage response = new CqMessage();
+
+            if (result is CqMessage resultMsg)
+            {
+                response.AddRange(resultMsg);
+            }
+            else
+            {
+                response.Add($"{result}");
+            }
+
+            if (ReplyInvoker)
+            {
+                if (AtInvoker)
+                {
+                    response.WithHead(new CqAtMsg(userId));
+                    response.WithHead(new CqTextMsg(" "));
+                    response.WithHead(new CqReplyMsg(messageId));
+                }
+                else
+                {
+                    response.WithHead(new CqReplyMsg(messageId));
+                }
+            }
+            else
+            {
+                if (AtInvoker)
+                {
+                    response.WithHead(new CqAtMsg(userId));
+                }
+            }
+
+            return response;
+        }
+
+        private CqMessage BuildPrivateResponse(object? result, long messageId)
+        {
+            CqMessage response = new CqMessage();
+
+            if (result is IEnumerable<CqMsg> resultMsgs)
+            {
+                response.AddRange(resultMsgs);
+            }
+            else
+            {
+                response.Add($"{result}");
+            }
+
+            if (ReplyInvoker)
+                response.WithHead(new CqReplyMsg(messageId));
+
+            return response;
+        }
+
         public async Task Execute(CqPostContext context, Func<Task> next)
         {
             if (context is CqMessagePostContext msgContext && msgContext.Message.Text.StartsWith(Prefix))
@@ -33,67 +91,38 @@ namespace EleCho.GoCqHttpSdk.CommandExecuting
                 string commandLine =
                     msgContext.Message.Text.Substring(Prefix.Length);
 
-                if (AllowGroupMessage && context is CqGroupMessagePostContext groupContext)
+                try
                 {
-                    try
+                    if (AllowGroupMessage && context is CqGroupMessagePostContext groupContext)
                     {
                         object? rst =
                             Execute(commandLine, GetStringComparison());
 
                         if (context.Session is ICqActionSession actionSession)
                         {
-                            CqMessage response = new CqMessage($"{rst}");
-
-                            if (ReplyInvoker)
-                            {
-                                if (AtInvoker)
-                                {
-                                    response.WithHead(new CqAtMsg(groupContext.UserId));
-                                    response.WithHead(new CqTextMsg(" "));
-                                    response.WithHead(new CqReplyMsg(msgContext.MessageId));
-                                }
-                                else
-                                {
-                                    response.WithHead(new CqReplyMsg(msgContext.MessageId));
-                                }
-                            }
-                            else
-                            {
-                                if (AtInvoker)
-                                {
-                                    response.WithHead(new CqAtMsg(groupContext.UserId));
-                                }
-                            }
+                            CqMessage response = 
+                                BuildGroupResponse(rst, groupContext.UserId, groupContext.MessageId);
 
                             await actionSession.SendGroupMessageAsync(groupContext.GroupId, response);
                         }
                     }
-                    catch (Exception exception)
-                    {
-                        await HandleExecutingException(context, exception);
-                    }
-                }
-                else if (AllowPrivateMessage && context is CqPrivateMessagePostContext privateContext)
-                {
-                    try
+                    else if (AllowPrivateMessage && context is CqPrivateMessagePostContext privateContext)
                     {
                         object? rst =
                             Execute(commandLine, GetStringComparison());
 
                         if (context.Session is ICqActionSession actionSession)
                         {
-                            CqMessage response = new CqMessage($"{rst}");
-
-                            if (ReplyInvoker)
-                                response.WithHead(new CqReplyMsg(msgContext.MessageId));
+                            CqMessage response =
+                                BuildPrivateResponse(rst, privateContext.MessageId);
 
                             await actionSession.SendPrivateMessageAsync(privateContext.UserId, response);
                         }
                     }
-                    catch (Exception exception)
-                    {
-                        await HandleExecutingException(context, exception);
-                    }
+                }
+                catch (Exception exception)
+                {
+                    await HandleExecutingException(context, exception);
                 }
 
                 if (ExecuteNextWhenExecuted)
