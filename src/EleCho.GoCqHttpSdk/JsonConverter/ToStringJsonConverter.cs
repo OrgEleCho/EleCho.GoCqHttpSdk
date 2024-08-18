35281c4a718e30ab4702ffab67830ac936163fac
ilyfairy <me@ilyfairy.com>
2024-08-18 18:21:33 +0800

    CqImageMsgDataModel.subType 数字转 string 避免异常

---
+++ src/EleCho.GoCqHttpSdk/JsonConverter/ToStringJsonConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace EleCho.GoCqHttpSdk.JsonConverter
+{
+    internal class ToStringJsonConverter : JsonConverter<string>
+    {
+        public override unsafe string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return reader.TokenType switch
+            {
+                JsonTokenType.String => reader.GetString(),
+                JsonTokenType.Number => Encoding.UTF8.GetString((byte*)Unsafe.AsPointer(ref Unsafe.AsRef(in reader.ValueSpan[0])), reader.ValueSpan.Length),
+                JsonTokenType.True => "true",
+                JsonTokenType.False => "false",
+                _ => throw new NotSupportedException(),
+            };
+        }
+
+        public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value);
+        }
+    }
+}
